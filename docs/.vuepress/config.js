69fe1e17b634df45a04f6d5031a151b0ad3866e5
xuliang <895252461@qq.com>
2022-05-14 01:19:03 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
     lang: 'zh-CN',
     title: 'Java 成神之路',
     description: '这是我的第一个 VuePress 站点',
-    base: "/note/",
+    base: "/",
 
     theme: defaultTheme({
         // 在这里进行配置
@@ -13,7 +13,17 @@ module.exports = {
                 text: 'Java 笔记',
 
                 children: [
-                   {
+                    {
+                        text: 'Java IO',
+                        children:[
+                            {
+                                text: 'Java IO 基础知识',
+                                link: '/java/java-io/Java IO 前置知识.md'
+                            }
+                        ]
+                    },
+
+                    {
                        text: 'Java 集合',
                        children:[
                            {
@@ -21,7 +31,46 @@ module.exports = {
                                link:"/Java/测试.md"
                            }
                        ]
-                   }
+                    },
+                    {
+                        text: 'Java 并发',
+                        children:[
+                            {
+                                text:'Java 并发基础',
+                                link:'/java/java-concurrent/Java并发基础.md'
+                            }
+                        ]
+                    },
+                    {
+                        text:'JVM 详解',
+                        children:[
+                            {
+                                text:'JVM概览',
+                                link:'/java/java-jvm/JVM概览.md'
+                            },
+                            {
+                                text:'JVM内存结构',
+                                link:'/java/java-jvm/JVM内存结构.md'
+                            },
+                            {
+                                text:'JVM垃圾回收',
+                                link:'/java/java-jvm/JVM垃圾回收.md'
+                            },
+                            {
+                                text:'JVM类文件结构',
+                                link:'/java/java-jvm/JVM类文件结构.md'
+                            },
+                            {
+                                text:'JVM类加载器',
+                                link:'/java/java-jvm/JVM类加载器.md'
+                            },
+                            {
+                                text:'Java内存模型',
+                                link:'/java/java-jvm/JVM内存模型.md'
+                            }
+                        ]
+                    }
+
 
                 ]
             },
@@ -42,27 +91,40 @@ module.exports = {
 
         ],
         sidebar: {
-            '/Java/': [
+            '/java/': [
                 {
-                    text: 'Guild',
-                    children:['/Java/测试.md']
+                    text: 'Java IO 模型',
+                    collapsible: true,
+                    children:[
+                        '/java/java-io/Java IO 前置知识.md',
+                        '/java/java-io/Java BIO.md'
+                    ]
                 },
                 {
-                    text: '进阶',
-                    children:['/Java/test01.md']
+                    text: 'Java 并发编程',
+                    collapsible: true,
+                    children:[
+                        '/java/java-concurrent/Java并发基础.md'
+                    ]
+                },
+                {
+                    text: 'JVM概览',
+                    collapsible: true,
+                    children:[
+                        '/java/java-jvm/JVM概览.md'
+                    ]
                 }
 
             ],
+           
             '/spring/':[
                 {
                     text: 'Spring Framework',
                     collapsible: true,
-                    
                     children:[
                         'test01',
                         'test02'
                     ]
-                    
                 }
             ]
         },
--- docs/README.md
+++ docs/README.md
@@ -1,3 +1,16 @@
-# Java 成神之路
-
-
+---
+home: true
+heroImage: /logo.png
+heroText: Java 成神之路.
+tagline: Born to win.
+actionText: 开始探索之旅 →
+actionLink: /zh/guide/
+features:
+- title: 关于我
+  details: 苦逼的程序员
+- title: 成神之路
+  details: 还在成神的路上爬着
+- title: 生活点点滴滴
+  details: 记录生活中美好的时刻，把生命中的点点滴滴都堆积成无法比拟的幸福。
+footer: Phoenix Licensed | Copyright © 2022-present Phoenix
+---
